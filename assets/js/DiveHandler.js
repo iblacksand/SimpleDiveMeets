e9d50e5c16c79abeaf3de4908801e12eeafe6928
John Elizarraras <chess4j@gmail.com>
2017-09-11 10:12:28 -0600

    Fixes Dive Search

--- assets/js/DiveHandler.js
+++ assets/js/DiveHandler.js
@@ -1,50 +1,39 @@
 // 'use strict';
 const Dive = require('./Dive.js');
 
-// function readTextFile(file, callback) {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, true);
-//     rawFile.onreadystatechange = function() {
-//         if (rawFile.readyState === 4 && rawFile.status == "200") {
-//             callback(rawFile.responseText);
-//         }
-//     };
-//     rawFile.send(null);
-// }
-fs = require('fs');
-var list;
-fs.readFile("./data/DiveList.json", function(err,data){
-    list = JSON.parse(data);
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+var list = [];
+readTextFile("./data/DiveList.json", function(text){
+    list = JSON.parse(text);
+    console.log(list);
 });
-// function readTextFile(file, callback) {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, true);
-//     rawFile.onreadystatechange = function() {
-//         if (rawFile.readyState === 4 && rawFile.status == "200") {
-//             callback(rawFile.responseText);
-//         }
-//     };
-//     rawFile.send(null);
-// }
-// var list;
-// readTextFile("./data/DiveList.json", function(text){
-//     list = JSON.parse(text);
-// });
 
 function getDiveList(){
     return list;
 }
 
 exports.getDive = function (code, boardHeight){
-    console.table(d[0]);
-    for(var d in list){
-        if(d["Dive Code"] === code.toUpper() && d["Board"] === boardHeight.toUpper()){
-            return d;
+    console.log("Code: " + code + "\nBoard: " + boardHeight);
+    for(let i = 0; i < list.length; i++){
+        console.log(list[i]["Dive Code"] + "\n" + list[i]["Board"]);
+        if(list[i]["Dive Code"] === code.toUpperCase() && list[i]["Board"] === boardHeight.toUpperCase()){
+            console.log(new Dive(list[i]));
+            return new Dive(list[i]);
         }
     }
     return new Dive({
         "Dive Description" : "ERROR"
     });
-}
+};
