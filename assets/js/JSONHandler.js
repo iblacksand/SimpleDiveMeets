5658326eda4b749bb4b349cdcba9a19f2af797b9
John Elizarraras <chess4j@gmail.com>
2017-09-21 10:38:05 -0600

    cleaning up comments and added documenetat

--- assets/js/JSONHandler.js
+++ assets/js/JSONHandler.js
@@ -1,9 +1,12 @@
+/**
+ * @param file This is the file name of the JSON file
+ * @return returns a promise with the object resolved
+ */
 module.exports.GetJSON = (file) => {
     return new Promise(resolve =>{
         var obj;
         readTextFile(file).then((text) =>{
             obj = JSON.parse(text);
-            // console.log("JSON OBJ FROM JSONHANDLER\n" + obj);
             resolve(obj);
         });
     });
@@ -11,13 +14,11 @@ module.exports.GetJSON = (file) => {
 
 function readTextFile(file) {
     return new Promise(resolve => {
-        console.log("file name: " + file);
         var rawFile = new XMLHttpRequest();
         rawFile.overrideMimeType("application/json");
         rawFile.open("GET", file, true);
         rawFile.onreadystatechange = function() {
             if (rawFile.readyState === 4 && rawFile.status == "200") {
-                console.log("text: " + rawFile.responseText);
                 resolve(rawFile.responseText);
             }
         };
