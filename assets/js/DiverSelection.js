7c41f8ecb53866c22690150444b2045277f8fc26
John Elizarraras <chess4j@gmail.com>
2018-02-18 12:44:42 -0700

    adds CreateMeet.html and button

--- assets/js/DiverSelection.js
+++ assets/js/DiverSelection.js
@@ -8,11 +8,11 @@ console.log('starting to get json');
 $('#eventselectbutton').on('click', () => {
     $('#diverSelectDiv').html("");
     JSONHandler.GetJSON("./data/meets/" + Toolbox.getParam("meet") + "/" + eventlist[$("#eventlist")[0].selectedIndex][0] + "/DiverList.json").then((obj) => {
-        let innerhtml = "<div class=\"container\"><center><div style=\"width:90%;\" class=\"select is-multiple\"><select style=\"width:90%;\" multiple size=\"" + obj.length + "\">";
+        let innerhtml = "<div style=\"width:90%;\" class=\"select is-multiple\"><select style=\"width:90%;\" multiple size=\"" + obj.length + "\">";
         for (let i = 0; i < obj.length; i++) {
             innerhtml += "<option value=\"" + obj[i] + "\">" + obj[i] + "</option>";
         }
-        innerhtml += "</select></div></center>";
+        innerhtml += "</select></div>";
         $("#diverSelectDiv").html(innerhtml);
     });
 });
--- main.js
+++ main.js
@@ -113,7 +113,11 @@ app.on('will-quit', () => {
 });
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    icon: path.join(__dirname, 'build/64x64.png')
+  });
 
   // and load the index.html of the app.
   win.loadURL(url.format({
