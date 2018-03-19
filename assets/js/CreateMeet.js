66a98958063e7c7fa0f5bf391d1ff3b3e1f19a39
John Elizarraras <chess4j@gmail.com>
2018-03-19 10:28:49 -0600

    adds remove buttons to events

--- assets/js/CreateMeet.js
+++ assets/js/CreateMeet.js
@@ -1,6 +1,24 @@
 var $ = require('jquery');
+var totalevents = 1;
 function addNewEvent(){
     let last = $('#last-eventdiv');
     last.removeAttr('id');
-    last.after('<div class="field"><div class="field has-addons" id="last-eventdiv"><div class="control is-expanded"><input class="input" type="text" placeholder="Event Name"></div><div class="control"><div class="select"><select><option>1M</option><option>3M</option></select></div></div></div></div>');
+    last.after('<div class="field"><div class="field has-addons" id="last-eventdiv" style="display:flex;justify-content:center;align-items:center;"><div class="control is-expanded"><input class="input" type="text" placeholder="Event Name"></div><div class="control"><div class="select"><select><option>1M</option><option>3M</option></select></div> <a class="delete" style="padding-left: 5pt" onclick="deleteEvent(' + totalevents + ')"></a></div></div></div>');
+    $("#last-eventdiv").attr('position', totalevents);
+    totalevents++;
+}
+
+function deleteEvent(index){
+    let toDelete = $("div[position='" + index +"']");
+    if(toDelete.attr("id") == "last-eventdiv") changeLast(index);
+    toDelete.remove();
+}
+
+function changeLast(index){
+    index--;
+    if($("div[position='" + index +"']").length){
+        $("div[position='" + index +"']").attr("id", "last-eventdiv");
+        return;
+    }
+    changeLast(index);
 }
