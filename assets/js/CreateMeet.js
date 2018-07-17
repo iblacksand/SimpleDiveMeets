5f337a802adfdfd8cd2b83c126f70255d2fac6a6
John Elizarraras <elizarrj27@gmail.com>
2018-07-16 22:56:42 -0600

    adds the beginnings of the rework

--- assets/js/CreateMeet.js
+++ assets/js/CreateMeet.js
@@ -1,5 +1,16 @@
 var $ = require('jquery');
+
 var totalevents = 1;
+
+function init(){
+    $("#name-page").show();
+    $("#event-page").hide();
+    $("#final-page").hide();
+}
+
+var liID = "#li1";
+
+init();
 
 /**
  * This creates a new event box that appears on CreateMeet.html
@@ -34,3 +45,31 @@ function changeLast(index){
     }
     changeLast(index);
 }
+
+function showPage(page){
+    var pos = ['#name-page', '#event-page', "#final-page"];
+    for(let i = 0; i < pos.length; i++){
+        if(pos[i] == page){
+            $(pos[i]).show();
+        }
+        else{
+            $(pos[i]).hide();
+        }
+    }
+}
+
+function makeActive(li){
+    $(liID).removeClass("is-active");
+    $(li).addClass("is-active");
+    liID = li;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var datePickers = bulmaCalendar.attach('[type="date"]', {
+      overlay: true,
+      minDate: '2018-01-01',
+      maxDate: '2018-12-31'
+    });
+    // datePickers now contains an Array of all datePicker instances
+  });
